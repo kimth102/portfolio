503383cafaef88c8502989e64c7185cdc6821319
김태희 <128497410+kimth102@users.noreply.github.com>
2023-07-17 17:45:30 +0900

    JS home section 페이지 아래로 스크롤 시 투명 처리

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
+// 페이지 아래로 스크롤 시 header 다크 스타일 적용
 const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
 
-// 페이지 아래로 스크롤 시 header 다크 스타일 적용
-function handleDocumentScroll() {
+document.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
   if (scrollY > headerHeight) {
     header.classList.add("header--dark");
   } else {
     header.classList.remove("header--dark");
   }
-}
+});
 
-document.addEventListener("scroll", handleDocumentScroll);
+// 페이지 아래로 스크롤 시 홈 화면 투명 적용
+const home = document.querySelector("#home__container");
+const homeHeight = home.offsetHeight;
+
+document.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  const opacity = 1 - scrollY / homeHeight;
+  home.style.opacity = opacity;
+});
